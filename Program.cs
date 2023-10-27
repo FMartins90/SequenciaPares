0e2e708da5e5edcc9dd92114452a7e9f7aa68657
Felipe <felipebamartins@hotmail.com>
2023-10-27 20:38:49 -0300

    Mostra sequencia de numeros Pares

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+//SequenciaPares
+int numero;
+int numeroPar = 0;
+
+do
+{
+    Console.Write("Digite um número inteiro e positivo: ");
+    //numero = Convert.Toint32(Console.ReadLine());
+    //utilizar apenas o código linha 11 ou código linha 13, possuem funcionalidade similar
+    Int32.TryParse(Console.ReadLine(), out numero);
+}
+while (numero <= 0);
+
+while (numeroPar <= numero)
+{
+    Console.Write($"{numeroPar} ");
+    numeroPar += 2;
+}
